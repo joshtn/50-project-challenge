91b3fe92595af1dde47665c7c48a5a0a99a30ef3
Josh Tan <prof.whale@gmail.com>
2020-12-13 15:46:15 +0100

    d9 soundboard

---
+++ d9-sound-board/script.js
@@ -0,0 +1,26 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    // no .stop, have to do the following
+    song.pause();
+    song.currentTime = 0;
+  });
+}
